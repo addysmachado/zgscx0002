348f380f80dac6ccb65b29b95534cc6b1527d3de
addysmachado <addys7@hotmail.com>
2016-09-02 20:31:39 -0500

    ajuste de librerias

--- recaudosDig/src/main/java/com/sap/recaudosdig/web/test.java
+++ recaudosDig/src/main/java/com/sap/recaudosdig/web/test.java
@@ -27,7 +27,7 @@ public class test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Esta es una prueba !!!");
+		response.getWriter().println("Esta es una prueba Get !!!");
 	}
 
 	/**
@@ -35,7 +35,7 @@ public class test extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Esta es una prueba !!!");
+		response.getWriter().println("Esta es una prueba Post !!!");
 	}
 
 }
