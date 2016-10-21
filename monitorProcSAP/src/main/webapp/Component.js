d1540936d370f8c3c02d6150bd80efe1a65cc037
addysmachado <addys7@hotmail.com>
2016-10-21 10:24:30 -0500

    Parametros de seleccion

---
+++ monitorProcSAP/src/main/webapp/Component.js
@@ -0,0 +1,33 @@
+/* eslint-disable max-params */
+sap.ui.define([
+	"sap/ui/core/UIComponent", "sap/ui/model/odata/v2/ODataModel", "sap/ui/model/resource/ResourceModel"
+], function(UIComponent, ODataModel, ResourceModel) {
+	"use strict";
+
+	var ROUTING_TARGET_CONTROL_ID = "ROOT";
+
+	var Component = UIComponent.extend("monitorProcSAP.Component", {
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function() {
+			UIComponent.prototype.init.apply(this, arguments);
+			// create the views based on the url/hash
+			this.getRouter().initialize();			
+			this._initModels();
+		},
+
+		_initModels: function() {
+
+			var oResourceModel = new ResourceModel({
+				bundleUrl: "i18n/i18n.properties"
+			});
+			sap.ui.getCore().setModel(oResourceModel,"i18n");
+
+		}
+	});
+
+	return Component;
+
+}, true /* export */);
---
+++ monitorProcSAP/src/main/webapp/jobs_app/view/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"monitorProcSAP/jobs_app/view/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("monitorProcSAP.jobs_app.view.App", {
+
+		onInit: function () {
+		}
+
+	});
+
+});
