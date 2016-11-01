a8b36b0956bc659f7175f2169dc20cce96950832
addysmachado <addys7@hotmail.com>
2016-11-01 09:03:46 -0500

    Versión de SM37

---
+++ zgscx0002/src/main/webapp/app/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"zgscx0002/app/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("zgscx0002.app.controller.App", {
+
+		onInit: function () {
+		}
+
+	});
+
+});
